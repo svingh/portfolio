5d2e29b07ceedb29a29c8c954055a44a9cd08ab5
svingh <117885644+svingh@users.noreply.github.com>
2025-05-23 01:56:55 -0400

    info update

--- src/Components/HeroModels/HeroExperience.jsx
+++ src/Components/HeroModels/HeroExperience.jsx
@@ -2,7 +2,7 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import { useMediaQuery } from 'react-responsive'
-import { Room } from './room'
+import { Room } from './Room'
 import HeroLights from './HeroLights'
 
 
